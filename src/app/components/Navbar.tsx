2f2383dd7ec051c3e42e39118809a43a6e67aa89
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-12-05 21:17:24 +0900

    feat: Hero section 70% 완료 및 Image에 priority 속성을 넣어줘서 LCP 향상

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,4 +1,5 @@
 'use client'
+
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
@@ -16,7 +17,7 @@ export default function Navbar() {
   return (
     <header className="mb-8 border-b">
       <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
-        <Link href={'/'}>
+        <Link href="/">
           <h1 className="text-2xl md:text-4xl font-bold">
             Modu<span className="text-primary">Buy</span>
           </h1>
@@ -46,7 +47,7 @@ export default function Navbar() {
 
         <div className="flex divide-x border-r sm:border-l">
           <Button
-            variant={'outline'}
+            variant="outline"
             className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none"
           >
             <ShoppingBag />
