83325cc5b4987bf7466220c6863a4aff92f3a6e5
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-12-06 21:22:09 +0900

    fix: params와 cateogry name이 달라서 이미지가 안나오는 에러 해결
    - 카테고리 이름은 Men 대문자인데 params이름은 men 소문자라 안나왔음

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -7,9 +7,9 @@ import { ShoppingBag } from 'lucide-react'
 
 const links = [
   { name: 'Home', href: '/' },
-  { name: 'Men', href: '/men' },
-  { name: 'Women', href: '/women' },
-  { name: 'Teens', href: '/teens' },
+  { name: 'Men', href: '/Men' },
+  { name: 'Women', href: '/Women' },
+  { name: 'Teens', href: '/Teens' },
 ]
 
 export default function Navbar() {
