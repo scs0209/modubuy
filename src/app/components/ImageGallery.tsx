e47a7fa769a2ef6af267c032b55304f677920eb7
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-12-06 20:31:23 +0900

    feat: ImageGallery 컴포넌트에서 Image 렌더링 완료

--- src/app/components/ImageGallery.tsx
+++ src/app/components/ImageGallery.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
-import { Image } from '../interface'
+import Image from 'next/image'
+import { Image as ProductImage } from '../interface'
+import { urlFor } from '../lib/sanity'
 
 interface Props {
-  images: Image[]
+  images: ProductImage[]
 }
 
 export default function ImageGallery({ images }: Props) {
-  return <div>ImageGallery</div>
+  return (
+    <div className="grid gap-4 lg:grid-cols-5">
+      <div className="order-last flex gap-4 lg:order-none lg:flex-col">
+        {images.map((image, idx) => (
+          <div
+            key={image._key}
+            className="overflow-hidden rounded-lg bg-gray-100"
+          >
+            <Image
+              src={urlFor(image).url()}
+              alt="product"
+              width={200}
+              height={200}
+              className="h-full w-full object-cover object-center cursor-pointer"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
