37b214e7d2c99200a140efa72467c0b88859b34d
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-12-06 20:41:39 +0900

    feat: ImageGallery 컴포넌트에서 main Image 섹션 완성 및 사진을 클릭하면 main이미지도 같이 변경되게 구현

--- src/app/components/ImageGallery.tsx
+++ src/app/components/ImageGallery.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import Image from 'next/image'
 import { Image as ProductImage } from '../interface'
 import { urlFor } from '../lib/sanity'
@@ -8,6 +10,12 @@ interface Props {
 }
 
 export default function ImageGallery({ images }: Props) {
+  const [mainImage, setMainImage] = useState(images[0])
+
+  const handleChangeImageClick = (image: ProductImage) => {
+    setMainImage(image)
+  }
+
   return (
     <div className="grid gap-4 lg:grid-cols-5">
       <div className="order-last flex gap-4 lg:order-none lg:flex-col">
@@ -15,6 +23,7 @@ export default function ImageGallery({ images }: Props) {
           <div
             key={image._key}
             className="overflow-hidden rounded-lg bg-gray-100"
+            onClick={() => handleChangeImageClick(image)}
           >
             <Image
               src={urlFor(image).url()}
@@ -25,6 +34,17 @@ export default function ImageGallery({ images }: Props) {
             />
           </div>
         ))}
+      </div>
+
+      {/* main-image */}
+      <div className="relative overflow-hidden rounded-lg bg-gray-100 lg:col-span-4">
+        <Image
+          src={urlFor(mainImage).url()}
+          alt="main-product"
+          width={500}
+          height={500}
+          className="h-full w-full object-cover object-center"
+        />
       </div>
     </div>
   )
