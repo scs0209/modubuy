1845b1de438d6c66c31c191b200f831781710e42
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-12-07 16:43:12 +0900

    fix: 안쓰는 코드 삭제

--- src/app/components/Providers.tsx
+++ src/app/components/Providers.tsx
@@ -2,7 +2,6 @@
 
 import React, { ReactNode } from 'react'
 import { CartProvider as USCProvider } from 'use-shopping-cart'
-import { backUrl } from '../config/url'
 
 export default function CartProvider({ children }: { children: ReactNode }) {
   return (
