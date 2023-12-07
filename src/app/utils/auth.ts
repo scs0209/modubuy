587823e98163b06d43f7adf834946f85d68e68db
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-12-07 19:51:57 +0900

    feat: prisma를 사용하여 supabase에 테이블 생성 완료

--- src/app/utils/auth.ts
+++ src/app/utils/auth.ts
@@ -1,11 +1,26 @@
 import { NextAuthOptions } from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
+import EmailProvider from 'next-auth/providers/email'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import prisma from './db'
 
 export const authOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }),
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
     }),
   ],
 } satisfies NextAuthOptions
