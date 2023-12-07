793dccd4da10eb5e030633b1dbf8a5b2e487f628
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-12-07 20:44:04 +0900

    fix: prisma generate 명령어 추가해서 배포 에러 해결

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { Toaster } from '@/components/ui/toaster'
 import Navbar from './components/Navbar'
 import CartProvider from './components/Providers'
 import ShoppingCartModal from './components/ShoppingCartModal'
@@ -23,6 +24,7 @@ export default function RootLayout({
         <CartProvider>
           <Navbar />
           <ShoppingCartModal />
+          <Toaster />
           {children}
         </CartProvider>
       </body>
