0ff41812a4ad1ede89b7e9085a0bd503740877b8
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-12-07 19:29:26 +0900

    feat: 로그인 기능 구현을 위해 next-auth 라이브러리 생성

---
+++ src/app/utils/auth.ts
@@ -0,0 +1,11 @@
+import { NextAuthOptions } from 'next-auth'
+import GithubProvider from 'next-auth/providers/github'
+
+export const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }),
+  ],
+} satisfies NextAuthOptions
