3f9e988d607af8152bc7667b04c5363b6e1f45c6
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-12-07 20:34:28 +0900

    feat: 로그인 기능 완성

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,39 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { getServerSession } from 'next-auth'
+import { redirect } from 'next/navigation'
+import { authOptions } from '../utils/auth'
+import SignInForm from '../components/auth/SignInForm'
+import SigninWithGithub from '../components/auth/SigninWithGithub.'
+
+export default async function LoginPage() {
+  const session = await getServerSession(authOptions)
+
+  if (session) {
+    return redirect('/')
+  }
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <Card>
+        <CardHeader>
+          <CardTitle>Please sign in </CardTitle>
+          <CardDescription>
+            To access the private page you have to be authenticated
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="flex flex-col">
+            <SignInForm />
+
+            <SigninWithGithub />
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
