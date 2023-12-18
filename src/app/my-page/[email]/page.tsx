e7071ba72f7f21f048dcdc0a462673883ea02bba
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-12-18 13:20:33 +0900

    feat: my-page에 내 결제 정보 데이터 불러오기 완료 및 더미데이터를 이용한 테이블 생성 완료

--- src/app/my-page/[email]/page.tsx
+++ src/app/my-page/[email]/page.tsx
@@ -1,6 +1,24 @@
+import UserTable from '@/app/components/mypage/UserTable'
+import { backUrl } from '@/app/config/url'
 import { authOptions } from '@/app/utils/auth'
 import { getServerSession } from 'next-auth'
 import React from 'react'
+
+async function fetchPayments(userId: string | unknown) {
+  try {
+    const response = await fetch(`${backUrl}/api/user?userId=${userId}`)
+    const paymentIds = await response.json()
+
+    paymentIds.forEach(async (paymentId: string) => {
+      // 두 번째 호출: Stripe 결제 정보 불러오기
+      const result = await fetch(`${backUrl}/api/payment/${paymentId}`)
+      const paymentInfo = await result.json()
+      console.log(paymentInfo)
+    })
+  } catch (error) {
+    console.error(error)
+  }
+}
 
 export default async function MyPage({
   params,
@@ -8,6 +26,14 @@ export default async function MyPage({
   params: { email: string }
 }) {
   const session = await getServerSession(authOptions)
+  const data = await fetchPayments(session?.user.id)
 
-  return <div>{session?.user.name}</div>
+  console.log(data)
+
+  return (
+    <div className="max-w-screen-lg mx-auto">
+      <div>{session?.user.name}</div>
+      <UserTable />
+    </div>
+  )
 }
