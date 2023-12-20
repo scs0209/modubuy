c5af6c204f6873ef3808c7396b29729f4b4ef189
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-12-20 18:04:46 +0900

    feat: admin 페이지 dashboard 디자인 완성

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Sidebar } from '../components/admin/SideBar'
+import Dashboard from '../components/admin/Dashboard'
 
 export default function AdminPage() {
   return (
     <div className="grid lg:grid-cols-5">
       <Sidebar />
+      <Dashboard />
     </div>
   )
 }
