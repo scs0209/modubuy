4b7d5e0e1e6eecaf30270e484a225495bd32ee45
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-12-20 17:43:52 +0900

    feat: admin 페이지 생성 및 sidebar 디자인 완성

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Sidebar } from '../components/admin/SideBar'
+
+export default function AdminPage() {
+  return (
+    <div className="grid lg:grid-cols-5">
+      <Sidebar />
+    </div>
+  )
+}
