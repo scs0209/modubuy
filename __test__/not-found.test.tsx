459d1fa084c2bd1fb25f9a45a1a857ad39429fd2
scs0209 <tjdckdtn2463@naver.com>
2024-05-10 23:14:13 +0900

    test: not-found 페이지 유닛 테스트 코드 작성

--- __test__/not-found.test.tsx
+++ __test__/not-found.test.tsx
@@ -7,6 +7,17 @@ describe('NotFound Page', () => {
     it('should have 404', () => {
       render(<NotFound />)
       expect(screen.getByText('404')).toBeInTheDocument()
+      expect(
+        screen.getByText('The page you’re looking for doesn’t exist.'),
+      ).toBeInTheDocument()
+    })
+
+    it('"Go Back" 링크가 홈으로 이동해야 함', () => {
+      render(<NotFound />)
+      expect(screen.getByRole('link', { name: /go back/i })).toHaveAttribute(
+        'href',
+        '/',
+      )
     })
   })
 })
