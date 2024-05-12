c7e667ac814085767c81a1b929ca818b575ba80a
scs0209 <tjdckdtn2463@naver.com>
2024-05-12 16:39:30 +0900

    test: Hero 컴포넌트 테스트 코드 작성 완료

--- __test__/Hero.test.tsx
+++ __test__/Hero.test.tsx
@@ -1,4 +1,5 @@
 import Hero from '@/app/_components/Hero'
+import { COMMON_PATH } from '@/constants/path'
 import { render, screen } from '@testing-library/react'
 
 describe('Hero Component', () => {
@@ -13,6 +14,25 @@ describe('Hero Component', () => {
           'We sell only the most exclusive and high quality products for you. We are the best so come and shop with us.',
         ),
       ).toBeInTheDocument()
+      expect(screen.getByText('Men')).toBeInTheDocument()
+      expect(screen.getByText('Women')).toBeInTheDocument()
+      expect(screen.getByText('Teens')).toBeInTheDocument()
+    })
+
+    it('링크가 정확하게 이동해야 함', () => {
+      render(<Hero />)
+      expect(screen.getByRole('link', { name: 'Men' })).toHaveAttribute(
+        'href',
+        COMMON_PATH.CATEGORY_MEN,
+      )
+      expect(screen.getByRole('link', { name: 'Women' })).toHaveAttribute(
+        'href',
+        COMMON_PATH.CATEGORY_WOMEN,
+      )
+      expect(screen.getByRole('link', { name: 'Teens' })).toHaveAttribute(
+        'href',
+        COMMON_PATH.CATEGORY_TEENS,
+      )
     })
   })
 })
